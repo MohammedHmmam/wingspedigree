447c7aa8359a3fcfae2cc4bf4c4ab63f660b6097
Hammam <mohammedhmmam@gmail.com>
2020-12-07 23:10:54 +0200

    Create Fancier model and migrate it

--- fancier/models.py
+++ fancier/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Fancier(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    birthDate = models.DateField(auto_now = True , auto_now_add=False)
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    region = models.CharField(max_length =30)
+    street = models.CharField(max_length = 100)
+    joinedDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
