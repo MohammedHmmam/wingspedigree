2cb5ee71ba7e922aff23abf5ccddb317ab6b26c2
eX <ex@ex.com>
2020-12-11 01:28:39 +0200

    Retrive specific pigeon data in a single page

--- pigeon/urls.py
+++ pigeon/urls.py
@@ -6,4 +6,5 @@ app_name = 'pigeon'
 
 urlpatterns = [
     url(r'^$' , views.pigeons_list , name='list'),
+    path('<slug:slug>/',views.pigeon_details , name = "details"),
 ]
--- pigeon/views.py
+++ pigeon/views.py
@@ -5,3 +5,9 @@ from . models import Pigeon
 def pigeons_list(request):
     pigeons = Pigeon.objects.all()
     return render(request , 'pigeon/pigeons_list.html', {'pigeons':pigeons})
+
+
+#Show specific Pigeon detaisl in single page
+def pigeon_details(request , slug):
+    pigeon = Pigeon.objects.get(slug=slug)
+    return render(request , 'pigeon/pigeon_details.html' , {'pigeon':pigeon})
